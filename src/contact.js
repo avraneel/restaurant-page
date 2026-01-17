6757a00b6daf53c7f6efc07ef7ed91f254164e45
Avraneel Pal <avraneelpal22@gmail.com>
2026-01-17 17:03:59 +0530

    add contact in external module

---
+++ src/contact.js
@@ -0,0 +1,16 @@
+export function createContact() {
+    const content = document.querySelector("#content");
+
+    const h2 = document.createElement("h2");
+    h2.textContent = "Contact Us"
+
+    const address = document.createElement("address");
+    const br = document.createElement("br");
+    address.append("123 Forest Road", br, "seafoodining@mail.com")
+
+    const contact = document.createElement("section");
+    contact.classList.toggle = "contact";
+    contact.append(h2, address);
+
+    content.appendChild(contact);
+}
